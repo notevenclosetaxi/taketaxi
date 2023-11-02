e49007d15b4af6fc19deea8a89eed77629698d12
johndoekim <amok0316@gmail.com>
2023-11-02 20:24:45 +0900

    경로 표시 테스트 완료

--- Tackxi/src/interface/route.interface.ts
+++ Tackxi/src/interface/route.interface.ts
@@ -21,3 +21,19 @@ export interface RouteInfoRes {
   data?: any;
   InfoList: object;
 }
+
+export interface Step {
+  stationList: string[];
+  mode: string;
+}
+
+export interface Route {
+  steps: Step[];
+}
+
+export interface StationInfo {
+  [key: string]: {
+    lat: string;
+    lon: string;
+  };
+}
